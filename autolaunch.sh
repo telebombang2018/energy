eae5809bbb58057ca167d2bec99c56d8a0b7278a
telebombang2018 <telebombang2018@gmail.com>
2017-06-22 10:14:46 -0700

    Delete autolaunch.sh

--- autolaunch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNTER=1
-while(true) do
-./tele_bom_bang_new.sh
-curl "https://api.telegram.org/bot379717771:AAFa7zve6nOgVnzggrcpOctPEf077mh-uag/sendmessage" -F "chat_id=310083510" -F "text=#NEWCRASH-#Tele_bom_bang-Reloaded-${COUNTER}-times"
-let COUNTER=COUNTER+1 
-done

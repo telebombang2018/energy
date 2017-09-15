3b96838bf9cac1bbb305818c5d7eec7ae684eb5e
telebombang2018 <telebombang2018@gmail.com>
2017-09-15 06:38:23 -0800

    energy

--- autoenergy.sh
+++ autoenergy.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-COUNTER=1
-while(true) do
-./energy.sh
-let COUNTER=COUNTER+1 
+while true ; do
+  for entr in energy.sh ; do
+    entry="${entr/.sh/}"
+    tmux kill-session -t $entry
+      rm -rf ~/.telegram-cli/data/sticker/*
+      rm -rf ~/.telegram-cli/data/photo/*
+      rm -rf ~/.telegram-cli/data/animation/*
+      rm -rf ~/.telegram-cli/data/video/*
+      rm -rf ~/.telegram-cli/data/audio/*
+      rm -rf ~/.telegram-cli/data/voice/*
+      rm -rf ~/.telegram-cli/data/temp/*
+      rm -rf ~/.telegram-cli/data/thumb/*
+      rm -rf ~/.telegram-cli/data/document/*
+      rm -rf ~/.telegram-cli/data/profile_photo/*
+      rm -rf ~/.telegram-cli/data/encrypted/*
+      rm -rf ./data/photos/*
+    tmux new-session -d -s $entry "./$entr"
+    tmux detach -s $entry
+  done
+  echo Bots Running!
+  sleep 1800
 done

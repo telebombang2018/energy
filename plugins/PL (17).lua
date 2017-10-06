ab360ec5a911aecd632033a7193c384548f835c3
telebombang2018 <telebombang2018@gmail.com>
2017-10-06 17:07:36 +1200

    ehsan

---
+++ plugins/PL (17).lua
@@ -0,0 +1,37 @@
+﻿local function delmsg (i,naji)
+    msgs = i.msgs 
+    for k,v in pairs(naji.messages_) do
+        msgs = msgs - 1
+        tdcli.deleteMessages(v.chat_id_,{[0] = v.id_}, dl_cb, cmd)
+        if msgs == 1 then
+            tdcli.deleteMessages(naji.messages_[0].chat_id_,{[0] = naji.messages_[0].id_}, dl_cb, cmd)
+            return false
+        end
+    end
+    tdcli.getChatHistory(naji.messages_[0].chat_id_, naji.messages_[0].id_,0 , 100, delmsg, {msgs=msgs})
+end
+local function run(msg, matches)
+    if matches[1] == 'del' and is_owner(msg) then
+        if tostring(msg.to.id):match("^-100") then 
+            if tonumber(matches[2]) > 1000 or tonumber(matches[2]) < 1 then
+                return  '🚫 *1000*> _تعداد پیام های قابل پاک سازی در هر دفعه_ >*1* 🚫'
+            else
+				tdcli.getChatHistory(msg.to.id, msg.id,0 , 100, delmsg, {msgs=matches[2]})
+				return "`"..matches[2].." `_پیام اخیر با موفقیت پاکسازی شدند_ 🚮"
+            end
+        else
+            return '⚠️ _این قابلیت فقط در سوپرگروه ممکن است_ ⚠️'
+        end
+    end
+end
+return {
+    patterns = {
+        '^[!#/]([Dd][Ee][Ll]) (%d*)$',
+    },
+    run = run
+}
+-----my_name_is_ehsan*#@mafia_boy
+-----@ENERGY_TEAM     FOR UPDATE
+-----لطفا پیام بالا رو پاک نکنید
+------منبع
+--Open By @LuaError
